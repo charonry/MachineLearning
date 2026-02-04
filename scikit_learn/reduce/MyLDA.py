a9b967d6e0864f42f0b1e6f308aeba572804cc94
charonry <1178807730@qq.com>
2026-02-04 15:08:22 +0800

    # scikit-learn中单变量特征提取LDA学习

---
+++ scikit_learn/reduce/MyLDA.py
@@ -0,0 +1,16 @@
+"""
+LDA:线性判别分析
+"""
+from sklearn.datasets import load_iris
+from sklearn.discriminant_analysis import LinearDiscriminantAnalysis
+from sklearn.preprocessing import StandardScaler
+
+iris = load_iris()
+X, y = iris['data'], iris['target']
+print(f"原始数据形状:{X.shape}")
+standard_scaler = StandardScaler()
+X_scaler = standard_scaler.fit_transform(X)
+lda = LinearDiscriminantAnalysis(n_components=2)
+X_lda = lda.fit_transform(X_scaler, y)
+print(f"降维后数据形状:{X_lda.shape}")
+print(f"降维后数据形状:{X_lda}")
