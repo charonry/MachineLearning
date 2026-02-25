af6a57df049d54ef8b7dc0acc60e7b0cb7aa945d
charonry <1178807730@qq.com>
2026-02-25 15:19:17 +0800

    # scikit-learn下模型加载和保存

---
+++ scikit_learn/loadsave/MyLoadModel.py
@@ -0,0 +1,19 @@
+import joblib
+from sklearn.datasets import load_iris
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, classification_report
+from sklearn.model_selection import train_test_split
+
+iris = load_iris()
+X = iris.data
+y = iris.target
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3)
+
+# 加载模型
+randomForest = joblib.load('rfc_model.pkl')
+
+y_pred = randomForest.predict(X_test)
+print(f"正确值：{y_test}\n随机森林预测值：{y_pred}")
+accuracy = accuracy_score(y_test, y_pred)
+print(f'测试集准确率：{accuracy:.4f}')
+print('分类报告：\n', classification_report(y_test, y_pred, target_names=iris.target_names))
